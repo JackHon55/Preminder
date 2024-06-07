abbcb0322c36115a3c5c1f2fd8057236f0d1317e
JackHon55 <jack_hon55@hotmail.com>
2024-06-07 17:02:19 +1000

    Initial Upload

--- PReminder/Form1.Designer.vb
+++ PReminder/Form1.Designer.vb
@@ -22,10 +22,120 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Med_List = New System.Windows.Forms.DataGridView()
+        Me.Med_name = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Med_repeat = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Med_Count = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Med_take = New System.Windows.Forms.DataGridViewButtonColumn()
+        Me.Med_buy = New System.Windows.Forms.DataGridViewButtonColumn()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Medtaken_text = New System.Windows.Forms.TextBox()
+        CType(Me.Med_List, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(149, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Prescription List"
+        '
+        'Med_List
+        '
+        Me.Med_List.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.Med_List.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Med_name, Me.Med_repeat, Me.Med_Count, Me.Med_take, Me.Med_buy})
+        Me.Med_List.Location = New System.Drawing.Point(12, 37)
+        Me.Med_List.MultiSelect = False
+        Me.Med_List.Name = "Med_List"
+        Me.Med_List.Size = New System.Drawing.Size(338, 257)
+        Me.Med_List.TabIndex = 2
+        '
+        'Med_name
+        '
+        Me.Med_name.HeaderText = "Name"
+        Me.Med_name.MinimumWidth = 100
+        Me.Med_name.Name = "Med_name"
+        '
+        'Med_repeat
+        '
+        Me.Med_repeat.HeaderText = "Repeats"
+        Me.Med_repeat.MinimumWidth = 60
+        Me.Med_repeat.Name = "Med_repeat"
+        Me.Med_repeat.Width = 60
+        '
+        'Med_Count
+        '
+        Me.Med_Count.HeaderText = "Count"
+        Me.Med_Count.MinimumWidth = 50
+        Me.Med_Count.Name = "Med_Count"
+        Me.Med_Count.Width = 50
+        '
+        'Med_take
+        '
+        Me.Med_take.HeaderText = ""
+        Me.Med_take.MinimumWidth = 40
+        Me.Med_take.Name = "Med_take"
+        Me.Med_take.Text = "Take"
+        Me.Med_take.UseColumnTextForButtonValue = True
+        Me.Med_take.Width = 40
+        '
+        'Med_buy
+        '
+        Me.Med_buy.HeaderText = ""
+        Me.Med_buy.MinimumWidth = 40
+        Me.Med_buy.Name = "Med_buy"
+        Me.Med_buy.Text = "Add"
+        Me.Med_buy.UseColumnTextForButtonValue = True
+        Me.Med_buy.Width = 40
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(14, 305)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Label2"
+        '
+        'Medtaken_text
+        '
+        Me.Medtaken_text.Location = New System.Drawing.Point(128, 302)
+        Me.Medtaken_text.Name = "Medtaken_text"
+        Me.Medtaken_text.Size = New System.Drawing.Size(222, 20)
+        Me.Medtaken_text.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(362, 329)
+        Me.Controls.Add(Me.Medtaken_text)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Med_List)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximumSize = New System.Drawing.Size(378, 368)
+        Me.MinimumSize = New System.Drawing.Size(378, 368)
+        Me.Name = "Form1"
+        Me.Text = "PReminder"
+        CType(Me.Med_List, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Med_List As DataGridView
+    Friend WithEvents Med_name As DataGridViewTextBoxColumn
+    Friend WithEvents Med_repeat As DataGridViewTextBoxColumn
+    Friend WithEvents Med_Count As DataGridViewTextBoxColumn
+    Friend WithEvents Med_take As DataGridViewButtonColumn
+    Friend WithEvents Med_buy As DataGridViewButtonColumn
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Medtaken_text As TextBox
 End Class
--- PReminder/Form1.vb
+++ PReminder/Form1.vb
@@ -1,3 +1,121 @@
-﻿Public Class Form1
+﻿Imports System.IO
 
+Public Class Form1
+    Public Medication_Array As New Dictionary(Of String, Medicine)
+    Public exeDirectory As String
+    Public savefile As String
+    Public prescription_data As String
+    Public lastdate As Date
+    Public lasttakenmed As String = ""
+
+    Public Class Medicine
+        Public repeat As Integer = 0
+        Public maxcount As Integer
+        Public count As Integer = 0
+    End Class
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        FileCheck_ExistorCreate()
+        Check_todayActivity()
+        Data_Read()
+        Populate()
+        Medtaken_text.Text = lasttakenmed
+    End Sub
+
+    Private Sub FileCheck_ExistorCreate()
+        ' looks if save file exist, else creates one
+        exeDirectory = Application.StartupPath
+        savefile = exeDirectory & "/Prescriptions.txt"
+
+        If File.Exists(savefile) Then
+            prescription_data = File.ReadAllText(savefile)
+        Else
+            ' If file doesn't exist, set last reminder date to current date
+            File.WriteAllText(savefile, $"{Date.Today}> >Med1;60;60;5|Med2;20;20;5")
+        End If
+    End Sub
+
+    Private Sub Check_todayActivity()
+        ' checks if preminder has been updated today
+        lastdate = Date.Parse(prescription_data.Split(">")(0))
+        Label2.Text = lastdate
+
+        If lastdate = Date.Today Then
+            lasttakenmed = prescription_data.Split(">")(1)
+        End If
+    End Sub
+
+    Private Sub Data_Read()
+        ' Read data
+        For Each xmed In prescription_data.Split(">")(2).Split("|")
+            Dim xname = xmed.Split(";")(0)
+            Dim xcount = xmed.Split(";")(1)
+            Dim xmaxcount = xmed.Split(";")(2)
+            Dim xrepeat = xmed.Split(";")(3)
+
+            Medication_Array.Add(xname, New Medicine With {.maxcount = xmaxcount, .count = xcount, .repeat = xrepeat})
+        Next
+    End Sub
+
+    Private Sub Populate()
+        ' Populate grid
+        For Each xobj As KeyValuePair(Of String, Medicine) In Medication_Array
+            Dim xrow As New DataGridViewRow()
+            Dim cell1 As New DataGridViewTextBoxCell() With {.Value = xobj.Key}
+            Dim cell2 As New DataGridViewTextBoxCell() With {.Value = xobj.Value.repeat}
+            Dim cell3 As New DataGridViewTextBoxCell() With {.Value = $"{xobj.Value.count}/{xobj.Value.maxcount}"}
+
+            xrow.Cells.Add(cell1)
+            xrow.Cells.Add(cell2)
+            xrow.Cells.Add(cell3)
+
+            Med_List.Rows.Add(xrow)
+        Next
+    End Sub
+
+    Private Sub Edit_Medicine(sender As DataGridView, e As DataGridViewCellEventArgs) Handles Med_List.CellClick
+        If e.ColumnIndex > 1 AndAlso TypeOf sender.Columns(e.ColumnIndex) Is DataGridViewButtonColumn Then
+            If e.ColumnIndex = 3 Then
+                Medicine_take(sender.Rows(e.RowIndex))
+            ElseIf e.ColumnIndex = 4 Then
+                Medicine_Add(sender.Rows(e.RowIndex))
+            End If
+        End If
+    End Sub
+
+    Public Sub Medicine_take(xrow As DataGridViewRow)
+        Dim oldcount As Integer = xrow.Cells(2).Value.Split("/")(0)
+        xrow.Cells(2).Value = $"{oldcount - 1}/{xrow.Cells(2).Value.Split("/")(1)}"
+        lastdate = Date.Today
+        Label2.Text = lastdate
+        Medtaken_text.Text = Medtaken_text.Text & $"{xrow.Cells(0).Value.Substring(0, 3)}"
+    End Sub
+
+    Public Sub Medicine_Add(xrow As DataGridViewRow)
+        xrow.Cells(1).Value += 1
+        lastdate = Date.Today
+        Label2.Text = lastdate
+    End Sub
+
+
+    Private Sub WindowClose(sender As Object, e As EventArgs) Handles Me.Closing
+        For Each xrow As DataGridViewRow In Med_List.Rows
+            If xrow.Cells(0).Value <> "" Then
+                Medication_Array(xrow.Cells(0).Value).repeat = xrow.Cells(1).Value
+                Medication_Array(xrow.Cells(0).Value).count = xrow.Cells(2).Value.Split("/")(0)
+            End If
+        Next
+
+        Dim newlines As New List(Of String)
+        For i As Integer = 0 To Medication_Array.Count - 1
+            Dim xname = Medication_Array.Keys(i)
+            Dim xcount = Medication_Array.Values(i).count
+            Dim xmaxcount = Medication_Array.Values(i).maxcount
+            Dim xrepeat = Medication_Array.Values(i).repeat
+
+            newlines.Add($"{xname};{xcount};{xmaxcount};{xrepeat}")
+        Next
+
+        File.WriteAllText(savefile, lastdate.ToString & ">" & Medtaken_text.Text & ">" & String.Join("|", newlines))
+    End Sub
 End Class
